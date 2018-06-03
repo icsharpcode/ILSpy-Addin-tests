6308bca26250fa8c9b3a4d333432a5eec3e49bb3
Andreas Weizel <rpinski@awzhome.de>
2018-06-04 00:14:22 +0200

    Added test projects for ILSpy.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Newtonsoft.Json.JsonConvert.SerializeObject(new List<string>());
+            var class1 = new Class1();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
