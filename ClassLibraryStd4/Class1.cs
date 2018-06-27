bb85d56c243affbda5027857aea67d45db3fd505
Andreas Weizel <rpinski@awzhome.de>
2018-06-28 01:20:09 +0200

    Added .NETStandard test project.

---
+++ ClassLibraryStd4/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibraryStd4
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            double test = 1.0f;
+            Console.WriteLine("Bla");
+        }
+    }
+}
