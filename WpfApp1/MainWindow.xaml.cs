56bc94ee7f2896a976887d050d399a04f3148cd4
Andreas Weizel <rpinski@awzhome.de>
2019-09-29 12:36:19 +0200

    Extend test projects with recent issue cases

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +27,9 @@ namespace WpfApp1
 
             var list = new List<string>();
             Console.WriteLine("Bla");
+
+            HttpClient graphClient = new HttpClient();
+            graphClient.DefaultRequestHeaders.Add("Authorization", "demovalue");
         }
     }
 }
